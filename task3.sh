ebc0e667f1d212bab631baab005889f91e184a51
nikhita003 <179535317+nikhita003@users.noreply.github.com>
2024-10-10 18:01:51 -0400

    Update task3.sh

--- task3.sh
+++ task3.sh
@@ -1,1 +1,1 @@
-cat titanic.csv | awk -F, '$3 == 2 && $13 == "S"' | sed 's/male/M/' | sed 's/female/F/' | awk -F, '{print $0} { sum += $7; count++ } END { if (count > 0) print "Average age : " sum/count }'
+cat titanic.csv | awk -F, '$3 == 2 && $13=="S"' | sed 's/male/M/' | sed 's/female/F/' | awk -F, '{print $0} { sum += $7; count++ } END { if (count > 0) print "Average age : " sum/count }'
