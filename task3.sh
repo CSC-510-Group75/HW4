700e9f450c648c0666685226f883d86c9f18666f
Manvija02 <88263337+Manvija02@users.noreply.github.com>
2024-10-24 16:44:04 +0000

    Adding the task3.sh correct code

--- task3.sh
+++ task3.sh
@@ -1,1 +1,6 @@
-cat titanic.csv | gawk -F, '$3 == 2 && $13 ~ /S/' | sed 's/female/F/' | sed 's/male/M/' | gawk -F\", '{print $2}' | gawk -F, '{ sum += $2; count++ } END { print sum / count}'
+cat titanic.csv | gawk -F, '$3 == 2 && $13 ~ /S/' | sed 's/female/F/' | sed 's/male/M/' | gawk -F, '{
+    if ($7 != "" && $7 ~ /^[0-9]+(\.[0-9]+)?$/) {
+        sum += $7;
+        count++;
+    }
+} END { if (count > 0) print(sum / count); }'
